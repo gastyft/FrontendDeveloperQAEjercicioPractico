3956bac0006ab111ceb5859807a0149b079a0eb4
gastyft <gastyftacla1@hotmail.com>
2024-03-21 08:54:08 -0300

    Created package Components where created Header, Principal and Error404 components and package Services. First configuration of app.routes.ts

--- FrontendDeveloperQA/src/app/app.routes.ts
+++ FrontendDeveloperQA/src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { NgModule } from '@angular/core';
+import { PrincipalComponent } from './components/principal/principal.component';
+import { Error404Component } from './components/error404/error404.component';
+export const routes: Routes = [
+    {path: '', 
+    redirectTo: 'app.component', pathMatch:'full'},
+     {path: 'principal', 
+     component:PrincipalComponent}, 
+  
+
+    { path: '**',  
+  component: Error404Component },
+  
+  ];
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
